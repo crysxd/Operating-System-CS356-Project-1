906303d280bb767350f6aecb756a8c0557a84964
crysxd <c.wuerthner@me.com>
2015-04-17 10:00:15 +0800

    Added raw structure for problem 9

---
+++ Problem 9/BurgerBuddies.c
@@ -0,0 +1,41 @@
+/*
+ * BurgerBuddies.c
+ * Author: Christian Würthner
+ * Description: Burger buddies problem.
+ */
+
+#define COOKS_COUNT 		3
+#define CASHIER_COUNT		2
+#define CUSTOMER_COUNT		4
+#define RACK_HOLDER_SIZE	4
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthreads.h>
+#include <stdbool.h>
+
+void cook_run();
+void cashier_run();
+void customer_run();
+
+int main(int argc, char **argv) {
+
+}
+
+void cook_run() {
+	while(1) {
+
+	}
+}
+
+void cashier_run() {
+	while(1) {
+
+	}
+}
+
+void customer_run() {
+	while(1) {
+
+	}
+}
